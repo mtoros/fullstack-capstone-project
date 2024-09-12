dff9a79af26bd1ea7870eb4ae0ab44c89df70cfe
mtoros <mtoros@gmail.com>
2024-09-12 16:07:40 +0200

    fixed login api

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,15 +1,67 @@
 import React, { useState } from 'react';
 import './LoginPage.css';
 
+import {urlConfig} from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
 function LoginPage() {
 
     //insert code here to create useState hook variables for email, password
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [incorrect, setIncorrect] = useState('');
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContext();
+
+    useEffect(() => {
+      if (sessionStorage.getItem('auth-token')) {
+        navigate('/app')
+      }
+    }, [navigate])
 
     // insert code here to create handleLogin function and include console.log
     const handleLogin = async () => {
         console.log("Inside handleLogin");
+        try{
+          //first task
+        const response = await fetch(`/api/auth/login`, {
+             //{{Insert code here}} //Task 7: Set method
+             method: 'POST',
+             //{{Insert code here}} //Task 8: Set headers
+             headers: {
+              'content-type': 'application/json',
+              'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
+            },
+             //{{Insert code here}} //Task 9: Set body to send user details
+             body: JSON.stringify({    
+              email: email,
+              password: password,
+            })
+       });
+
+       if (json.authtoken) {
+        const json = await res.json();
+        sessionStorage.setItem('auth-token', json.authtoken);
+        sessionStorage.setItem('name', json.userName);
+        sessionStorage.setItem('email', json.userEmail);
+        setIsLoggedIn(true);
+        navigate('/app')
+          } else {
+            document.getElementById("email").value="";
+            document.getElementById("password").value="";
+            setIncorrect("Wrong password. Try again.");
+        //Below is optional, but recommended - Clear out error message after 2 seconds
+            setTimeout(() => {
+              setIncorrect("");
+            }, 2000);
+          }
+
+
+        }catch (e) {
+          console.log("Error fetching details: " + e.message);
+      }
     }
 
         return (
@@ -30,6 +82,7 @@ function LoginPage() {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
             />
+            <span style={{color:'red',height:'.5cm',display:'block',fontStyle:'italic',fontSize:'12px'}}>{incorrect}</span>
           </div>
 
           {/* insert code here to create a button that performs the `handleLogin` function on click */}
